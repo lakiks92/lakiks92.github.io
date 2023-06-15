123199031640cc0636932d80a2b4d2f545253866
lakiks92 <136716085+lakiks92@users.noreply.github.com>
2023-06-15 21:47:38 +0200

    Contactez nous

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formbold.com/s/3VpAA",
+        url: "https://formbold.com/s/9XxRy",
         type: "POST",
         data: {
           name: name,
